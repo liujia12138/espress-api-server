f2a2ff4f5139ecd8930885649728fc4857403fc2
liujia <liujia12101@163.com>
2022-07-08 11:31:21 +0800

    密码加密，插入新用户

--- router_handler/user.js
+++ router_handler/user.js
@@ -3,6 +3,9 @@
 // 导入数据库操作模块
 const db = require('../db')
 
+// 导入加密模块
+const bcrypt = require('bcryptjs')
+
 // 注册
 // 步骤
 // 1.检测表单提交的数据是否合法
@@ -13,7 +16,7 @@ exports.registerUser = (req, res) => {
   // 接收数据
   const userinfo = req.body
   console.log(userinfo)
-  // 检测数据的合法性
+  // 1.检测数据的合法性
   if (!userinfo.username || !userinfo.password) {
     return res.send({
       status: 1,
@@ -21,7 +24,7 @@ exports.registerUser = (req, res) => {
     })
   }
 
-  // 检测用户名是否被占用
+  // 2.检测用户名是否被占用
   const sql = 'select * from ev_users where username=?'
   db.query(sql, [userinfo.username], (err, results) => {
     // sql语句执行失败
@@ -31,7 +34,23 @@ exports.registerUser = (req, res) => {
     if (results.length > 0) {
       return res.send({ status: 1, msg: '用户名被占用，请更换一个用户名' })
     }
-    res.send('注册成功')
+
+    // 用户名可用
+    // 3.密码加密，调用bcrypt.hashSync(明文密码, 随机盐的长度)
+    const pwd = bcrypt.hashSync(userinfo.password, 10)
+    console.log(pwd)
+
+    // 4.插入新用户
+    const sql = 'insert into ev_users set ?'
+    db.query(sql, { username: userinfo.username, password: pwd }, (err, results) => {
+      // err
+      if (err) return res.send({ status: 1, msg: err.message })
+
+      // 插入失败
+      if(results.affectedRows !== 1) return res.send({status:1, msg:'注册失败，请稍后再试'})
+
+      res.send({status: 0, msg: '注册成功'})
+    })
   })
 }
 
